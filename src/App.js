dff42eb92a8023fc7b73f573f4051f4368a8d22c
“Azmi <Azmitammam@gmail.com>
2022-02-18 15:28:13 +0200

    Add Restaurant Menu Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./style.sass";
-import 'animate.css';
+import "animate.css";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Partner from "./components/Partner/Partner";
-import ApplyPartner from "./components/Partner/ApplyPartner"
+import ApplyPartner from "./components/Partner/ApplyPartner";
 import Rider from "./components/Rider/Rider";
 import Footer from "./components/Footer/Footer";
 import PartnerPanel from "./components/Partner/PartnerPanel/PartnerPanel";
@@ -15,26 +15,27 @@ import Sidebar from "./components/Admin/sidebar/sidebar";
 import Users from "./components/Admin/Users/users";
 import Profile from "./components/Profile/Profile";
 import Restaurants from "./components/Restaurants/restaurants";
+import RestaurantDetails from "./components/RestaurantDetails/RestaurantDetails";
 
 function App() {
   return (
     <BrowserRouter>
-          <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Rider" element={<Rider />} />
-          <Route path="/Partner" element={<Partner />} />
-          <Route path="/Restaurants" element={<Restaurants />} />
-          <Route path="/ApplyPartner" element={<ApplyPartner />} />
-          <Route path="/PartnerPanel" element={<PartnerPanel />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/sidebar" element={<Sidebar />} />
-          <Route path="/Users" element={<Users />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/*" element={<PageNotFound />} />
-          </Routes>
-
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Rider" element={<Rider />} />
+        <Route path="/Partner" element={<Partner />} />
+        <Route path="/Restaurants" element={<Restaurants />} />
+        <Route path="/RestaurantMenu" element={<RestaurantDetails />} />
+        <Route path="/ApplyPartner" element={<ApplyPartner />} />
+        <Route path="/PartnerPanel" element={<PartnerPanel />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+        <Route path="/Users" element={<Users />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 }
