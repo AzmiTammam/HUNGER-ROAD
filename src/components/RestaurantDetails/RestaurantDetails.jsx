e7c79dcd259d142d8652c4b1aa77bb35d2911ee9
“Azmi <Azmitammam@gmail.com>
2022-02-18 15:28:33 +0200

    Restaurant Details Component

---
+++ src/components/RestaurantDetails/RestaurantDetails.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./RestaurantDetails.css";
+import Navbar from "../navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+function RestaurantDetails() {
+  return (
+    <>
+      <Navbar />
+      <div className="RestaurantDetails">
+        h <br />
+        h <br />
+        h <br />
+        h <br />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default RestaurantDetails;
