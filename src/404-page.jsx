94501326eb7f963a46bc68ac4c48f6c72d89b09d
“Azmi <Azmitammam@gmail.com>
2022-02-23 15:01:40 +0200

    Change on 404 pages

--- src/404-page.jsx
+++ src/404-page.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import notFound from './images/notFound.svg';
-import '../src/style.sass';
-import Navbar from './components/navbar/Navbar';
+import React from "react";
+import notFound from "./images/notFound.svg";
+import "../src/style.sass";
+import Navbar from "./components/navbar/Navbar";
 
 const PageNotFound = () => {
-    return (
-        <div className="notFound404">
-                  <Navbar />
-            <div className="info">
-                <h1>404</h1>
-                <h2>This page could not be found</h2>
-            <img src={notFound} alt="This page could not be found" />
-            </div>
-        </div >
-    )
-}
+  return (
+    <div className="notFound404">
+      <Navbar />
+      <div className="info">
+        <h1>404</h1>
+        <h2>This page could not be found!</h2>
+        <img src={notFound} alt="This page could not be found!" />
+      </div>
+    </div>
+  );
+};
 
 export default PageNotFound;
