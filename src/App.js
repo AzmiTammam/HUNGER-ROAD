806b3c9da6545fc804a2830d8e1e99798287014e
“Azmi <Azmitammam@gmail.com>
2022-02-23 15:09:47 +0200

    Edit 404 Route Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./style.sass";
 import "animate.css";
-import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Partner from "./components/Partner/Partner";
 import ApplyPartner from "./components/Partner/ApplyPartner";
 import Rider from "./components/Rider/Rider";
-import Footer from "./components/Footer/Footer";
 import PartnerPanel from "./components/Partner/PartnerPanel/PartnerPanel";
 import Admin from "./components/Admin/admin";
-import PageNotFound from "./404-page";
+import PageNotFound from "./Page/404-page";
 import Sidebar from "./components/Admin/sidebar/sidebar";
 import Users from "./components/Admin/Users/users";
 import Profile from "./components/Profile/Profile";
